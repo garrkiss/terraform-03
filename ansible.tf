ca5aa910f0a2934dad7b8e1fca9683ad0017564a
garrkiss <123040451+garrkiss@users.noreply.github.com>
2024-12-09 14:54:19 +0300

    Update ansible.tf

--- ansible.tf
+++ ansible.tf
@@ -1,6 +1,6 @@
 resource "local_file" "inventory" {
     depends_on = [yandex_compute_instance.web, yandex_compute_instance.db, yandex_compute_instance.storage]
-    filename = "./hosts.yml"
+    filename = "./hosts.ini"
     content = templatefile("./ansible.tftpl", {
         webservers = [yandex_compute_instance.web],
         databases = [yandex_compute_instance.db],
